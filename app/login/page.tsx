3f793890edfe74f46d43f0646dda67d1ecc1b4f4
Taranpreet Singh <24ba022@srcc.edu>
2026-02-08 16:40:17 +0530

    Center AuthForm in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,5 +3,9 @@
 import AuthForm from "@/features/auth/components/AuthForm";
 
 export default function LoginPage() {
-  return <AuthForm />;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <AuthForm />
+    </div>
+  );
 }
