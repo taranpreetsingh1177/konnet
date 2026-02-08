a90d76e9082b91f825e24bec6179cbdede7a45b1
Taranpreet Singh <24ba022@srcc.edu>
2026-02-08 21:37:33 +0530

    Update login page styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,8 +4,8 @@ import AuthForm from "@/features/auth/components/AuthForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-gray-50">
-      <div className="w-full max-w-md p-4">
+    <div className="flex h-screen w-full items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md p-4 ">
         <AuthForm />
       </div>
     </div>
